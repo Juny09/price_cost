1dd7332710061da2abb715365bf80b5b624c5635
junyuan <junyuan13915@gmail.com>
2026-09-08 17:58:05 +0800

    update app.py

--- app.py
+++ app.py
@@ -30,6 +30,6 @@ def calculate():
         "selling_price": round(selling_price, 2)
     })
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
